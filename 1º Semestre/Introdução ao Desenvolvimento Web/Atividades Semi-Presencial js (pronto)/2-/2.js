b00af51b3dae59944c3f22cbdf1c939020834f12
Felipe Da Rosa <101990417+Felipedarosaa@users.noreply.github.com>
2023-03-07 12:42:07 -0300

    Delete 1º Semestre directory

--- 1º Semestre/Introdução ao Desenvolvimento Web/Atividades Semi-Presencial js (pronto)/2-/2.js
+++
@@ -1,9 +0,0 @@
-//Some dois valores lidos e imprima o resultado
-function somarNum() {
-    var numero = parseFloat((document.getElementById('numero').value));
-    var numero2 = parseFloat((document.getElementById('numero2').value));
-
-    var soma = numero + numero2;
-    document.getElementById('resultado').innerHTML = 'A soma é ' + soma;
-
-}
